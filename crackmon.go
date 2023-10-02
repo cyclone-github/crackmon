c8abda7239390be2ed1a94e7127e42bdb6cbae5f
cyclone <34043806+cyclone-github@users.noreply.github.com>
2023-10-02 10:35:43 -0500

    v2023-10-02.1030-debug
    v2023-10-02.1030-debug; fixed timeThreshold; added more debugging info

--- crackmon.go
+++ crackmon.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"regexp"
@@ -29,19 +30,20 @@ example:
 
 version:
 v2023-10-01.1700; initial github release
+v2023-10-02.1030-debug; fixed timeThreshold; added more debugging info
 */
 
 func help() {
 	version()
 	fmt.Fprintln(os.Stderr, "\nExamples:\n")
-	fmt.Fprintln(os.Stderr, "(Defaults to -time 1m -crack 1)")
+	fmt.Fprintln(os.Stderr, "(Defaults to -time 5m -crack 1)")
 	fmt.Fprintln(os.Stderr, "./crackmon ./hashcat {hashcat args}\n")
 	fmt.Fprintln(os.Stderr, "Custom: -time 10m -crack 2")
 	fmt.Fprintln(os.Stderr, "./crackmon -t 10 -c 2 ./hashcat {hashcat args}\n")
 }
 
 func version() {
-	fmt.Fprintln(os.Stderr, "crackmon v2023-10-01.1700; initial github release")
+	fmt.Fprintln(os.Stderr, "crackmon v2023-10-02.1030-debug")
 	fmt.Fprintln(os.Stderr, "https://github.com/cyclone-github/crackmon")
 	fmt.Fprintln(os.Stderr, "Original idea by: https://github.com/justpretending/avgdrop")
 	detectedOS := checkOS()
@@ -59,7 +61,7 @@ func checkOS() string {
 }
 
 func main() {
-	timeThreshold := flag.Int("t", 1, "Time threshold in minutes")
+	timeThreshold := flag.Int("t", 5, "Time threshold in minutes")
 	cracksThreshold := flag.Int("c", 1, "Cracks per time threshold")
 	cycloneFlag := flag.Bool("cyclone", false, "Display message")
 	versionFlag := flag.Bool("version", false, "Display version")
@@ -107,6 +109,9 @@ func main() {
 
 	// print the full command to be executed
 	fmt.Fprintln(os.Stderr, "\nExecuting:", cmdStr+"\n")
+	fmt.Fprintf(os.Stderr, "DEBUG: timeThreshold = %d\n", *timeThreshold)
+	fmt.Fprintf(os.Stderr, "DEBUG: cracksThreshold = %d\n", *cracksThreshold)
+	time.Sleep(1 * time.Second)
 
 	// split the full command string into command and arguments
 	cmdSlice := strings.Fields(cmdStr)
@@ -126,7 +131,7 @@ func main() {
 
 	scanner := bufio.NewScanner(stdout)
 	re := regexp.MustCompile(`CUR:(\d+),`)
-	counter := 0
+	//counter := 0
 
 	// "Recovered/Time" var
 	var recoveredTimeSeen bool = false
@@ -135,13 +140,15 @@ func main() {
 	var lineEnding string
 	if detectedOS == "windows" {
 		lineEnding = "\r\n"
+		//lineEnding = "\n"
 	} else {
 		lineEnding = "\n"
 	}
 
 	// monitor hashcat output
 	go func() {
-		fmt.Fprintf(os.Stderr, "Debug: Starting on OS: %s\n\n", detectedOS)
+		fmt.Fprintf(os.Stderr, "DEBUG: Starting on OS: %s\n\n", detectedOS)
+		lastCrackTime := time.Now() // start time
 		for scanner.Scan() {
 			line := scanner.Text()
 			fmt.Println(line) // print full hashcat command -- debugging
@@ -149,20 +156,23 @@ func main() {
 			if len(matches) > 1 {
 				cracks, _ := strconv.Atoi(matches[1])
 				if cracks < *cracksThreshold {
-					counter++
-					if counter >= *timeThreshold {
-						fmt.Fprintln(os.Stderr, "\nDebug: Hashcat bypass sent\n")
-						stdin.Write([]byte("b" + lineEnding))
-						counter = 0
+					elapsed := time.Since(lastCrackTime).Minutes() // get elapsed time in minutes
+					if int(elapsed) >= *timeThreshold {
+						fmt.Fprintf(os.Stderr, "\nDEBUG: cracks = %d, elapsed = %f, timeThreshold = %d\n", cracks, elapsed, *timeThreshold)
+						fmt.Fprintln(os.Stderr, "DEBUG: Hashcat bypass ('b') sent\n")
+						io.WriteString(stdin, "b"+lineEnding) // io.WriteString works on linux and windows
+						time.Sleep(1 * time.Second)           // add delay (testing: for windows)
+						lastCrackTime = time.Now()            // reset crack time
 					}
 				} else {
-					counter = 0
+					lastCrackTime = time.Now() // reset crack time
 				}
 			}
 
 			if strings.Contains(line, "Recovered/Time") {
 				recoveredTimeSeen = true
 			}
+			time.Sleep(10 * time.Millisecond) // add delay
 		}
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "stdin error:", err) // debug
@@ -171,27 +181,35 @@ func main() {
 
 	// debugging goroutine
 	go func() {
+		time.Sleep(10 * time.Second) // add delay to allow hashcat to start
 		ticker := time.NewTicker(10 * time.Second)
 		missedChecks := 0 // counter for missed checks
 		for range ticker.C {
+			fmt.Fprintln(os.Stderr, "DEBUG: Ticker: OK")
+
 			if !recoveredTimeSeen {
-				fmt.Fprintln(os.Stderr, "Debug: Hashcat output: FALSE\n")
 				missedChecks++
+				fmt.Fprintf(os.Stderr, "DEBUG: missedChecks: %d\n", missedChecks)
+				fmt.Fprintln(os.Stderr, "DEBUG: Hashcat output: FALSE\n")
+
 				if missedChecks >= 3 {
-					fmt.Fprintln(os.Stderr, "Debug: Stopping Hashcat due to 3 consecutive missed checks.\n")
-					stdin.Write([]byte("q" + lineEnding))
+					fmt.Fprintln(os.Stderr, "DEBUG: Stopping Hashcat due to 3 consecutive missed checks.")
+					fmt.Fprintln(os.Stderr, "DEBUG: Program must be able to read hashcat output: 'Recovered/Time...: CUR:'\n")
+					io.WriteString(stdin, "q"+lineEnding) // io.WriteString works on linux and windows
+					time.Sleep(1 * time.Second)           // add delay (testing: for windows)
 				}
 			} else {
-				fmt.Fprintln(os.Stderr, "Debug: Hashcat output: OK\n")
+				fmt.Fprintln(os.Stderr, "DEBUG: Hashcat output: OK\n")
 				missedChecks = 0
 			}
+			time.Sleep(100 * time.Millisecond) // add delay
 		}
 	}()
 
 	// wait for command to finish
 	err = cmd.Wait()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "hashcat command error:", err)
+		fmt.Fprintln(os.Stderr, "hashcat status:", err)
 	}
 }
 
